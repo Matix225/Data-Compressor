f1501e7523e9b07ee7f4b1c6499e61f03aa3e7ff
Matix225 <mateusz.fiedorczuk@gmail.com>
2026-04-04 23:50:10 +0200

    Complete huffman encoding function

--- huffman.cpp
+++ huffman.cpp
@@ -3,6 +3,21 @@
 #include <map>
 using namespace std;
 
+map<char, string> codes;
+
+void build_codes(huff_node *root, string code = ""){
+    if(root->l_child == NULL && root->r_child == NULL){
+        if(code == "") //file contains only 1 unique character
+            codes[root->sign] = "0";
+        else
+            codes[root->sign] = code;
+        
+        return;
+    }
+
+    build_codes(root->l_child, code + '0');
+    build_codes(root->r_child, code + '1');
+}
 void huffman(ifstream &file, string op){ //algorythm entry point
     if(op == "Encode" || op == "encode")
         encode(file);
@@ -20,6 +35,10 @@ void encode(ifstream &file){
     while(file.get(sign))
         tab[sign]++;
 
+    if(tab.empty()){//File was empty. There is nothing to compress.
+        cout<<"You can't compress empty file!\n";
+        return;
+    }
 
     //create single nodes for every character in file
     prior_q queue;
@@ -31,6 +50,7 @@ void encode(ifstream &file){
         queue.insert(node);
     }
 
+    //create huffman tree
     huff_node *a, *b, *tmp = NULL;
     while(queue.size > 1){
         a = queue.extract_min();
@@ -43,4 +63,42 @@ void encode(ifstream &file){
         
         queue.insert(tmp);
     }
+
+    huff_node *root = queue.tab[1];
+    codes.clear(); //clear code map after last encode
+    build_codes(root);
+
+    file.clear(); //reset all state bits (EOF etc.)
+    file.seekg(0); //go back to the begining of the file
+
+    char s; 
+    unsigned char buff = 0;
+    int bit_count = 0;
+    ofstream compF("compressed.huff", ios::binary);
+    if(!compF.good())
+        return;
+
+    while(file.get(s)){
+        string code = codes[s];
+        for(int i = 0; i < (int)code.size(); i++){
+            if(code[i] == '0')
+                buff = buff << 1;
+            else{
+                buff = buff << 1;
+                buff = buff | 1;
+            }
+
+            bit_count++;
+            if(bit_count == 8){
+                compF.write((char*)&buff, 1);
+                buff = 0;
+                bit_count = 0;
+            }
+        }
+    }
+    if(bit_count > 0){
+        buff <<= (8 - bit_count);
+        compF.write((char*)&buff, 1);
+    }
+    compF.close();
 }
